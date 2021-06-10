ee9a8e4483cfa7ecd6a6040901072446a340e25f
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-06-11 03:04:19 +0600

    server

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import './Inventory.modules.css';
 
 const Inventory = () => {
+
+
+    const handleAddProduct = () => {
+
+        const product = {}
+
+        fetch('https://lit-cove-35084.herokuapp.com/addProduct', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+    }
+
     return (
         <div>
-            <h1>Inventory coming soon...</h1>
+
+
+
+            <form className="ship-form">
+                <input placeholder="Product Name" />
+
+                <input type="text" placeholder="Product Price" />
+
+                <input type="text" placeholder="Product quantity" />
+
+                <input type="file" placeholder="Upload Product" />
+
+                <input type="submit" />
+
+            </form>
+
+
+
+
+
         </div>
     );
 };
--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 
 const ProductDetail = () => {
-    const {productKey} = useParams();
-    const product = fakeData.find(pd => pd.key === productKey);
-    
+    const { productKey } = useParams();
+    const [product, setProduct] = useState({})
+
+
+
+
+
+    useEffect(() => {
+
+        fetch('https://lit-cove-35084.herokuapp.com/product/' + productKey)
+            .then(res => res.json())
+            .then(data => {
+                setProduct(data)
+            })
+    }, [productKey])
+
+
     return (
         <div>
             <h1>Your Product Details.</h1>
