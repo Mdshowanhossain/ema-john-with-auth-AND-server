aaec99f7588d2ade26aab1b9dd3a33b340b84b21
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-06-10 14:44:48 +0600

    with auth

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNDa8TeQkTCjN_m6pcZ8mqXTIMV1M_ogg",
+  authDomain: "ema-john-shopping-cart-4c1e9.firebaseapp.com",
+  projectId: "ema-john-shopping-cart-4c1e9",
+  storageBucket: "ema-john-shopping-cart-4c1e9.appspot.com",
+  messagingSenderId: "673003090635",
+  appId: "1:673003090635:web:cf554ab3278981a5b3214e"
+};
+export default firebaseConfig;
